144f77dfda401be3650a8b80d5fbf12ca688d33b
Kaniz Fatima <kfatima1204@gmail.com>
2026-09-19 15:37:49 +0600

    fix: use @vite() directive, fix LF line endings in start.sh, add .gitattributes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="{{ asset('build/assets/app-I9PsVGag.css') }}">
-    <script type="module" src="{{ asset('build/assets/app-MNQzeqcZ.js') }}" defer></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
 </head>
 <body>
